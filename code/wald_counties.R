47333dd90b7584501bb853cfb408c23bb38b417a
rachaelaber <57012430+rachaelaber@users.noreply.github.com>
2023-05-25 21:02:20 -0700

    extend p-value computation to all US counties

--- code/wald_counties.R
+++ code/wald_counties.R
@@ -1,25 +1,24 @@
-# Compute p-values for a state's counties 
+# Compute p-values for counties 
 source("code/W.R")
 
 filename <- "data/processed_dat.RData"
 load(filename)
 rm(filename)
 
-# Loop through counties and calculate p values
-state_index <- which(populations$State == state)
+# Loop through counties and calculate p-values
 
-pvals <- rep(NA, times = length(state_index))
+pvals <- rep(NA, times = dim(new_cases_subset)[1])
 
-for (i in 1:length(state_index)){
-  pvals[i] <- tryCatch(W(new_cases_subset[state_index[i],], 
-                         population_size = populations$population[state_index[i]],
+for (i in 1:dim(new_cases_subset)[1]){
+  pvals[i] <- tryCatch(W(new_cases_subset[i,], 
+                         population_size = populations$population[i],
                          breakpoint = 30, deg_free = 3, 
                          fn = my_spl_fit, verbose = FALSE), 
                        error = function(e) return(NA))
 }
 
 # Save p-values
-filename <- paste("data/W_pvals", state, ".Rdata", sep ="")
+filename <- "data/W_pvals"
 save(pvals, file = filename)
 
 # Visualize p-values

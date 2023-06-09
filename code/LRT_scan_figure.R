4bd52a2f584216c90d4d109609c017a82663ba1c
rachaelaber <57012430+rachaelaber@users.noreply.github.com>
2023-06-09 13:11:35 -0700

    update figures

--- code/LRT_scan_figure.R
+++ code/LRT_scan_figure.R
@@ -9,20 +9,21 @@ test <- new_cases[1,]
 lrt_stat <- c()
 
 for (i in 30:(length(test) - 30 + 1)){
-  lrt_stat <- c(lrt_stat, lrt(y1 = test[(i - 29):i], y2 = test[(i + 1):(i + 30)], 
+  lrt_stat <- c(lrt_stat, tryCatch(lrt(y1 = test[(i - 29):i], y2 = test[(i + 1):(i + 30)], 
                            s1 = populations$population[1], 
                            s2 = populations$population[1],
                            i1 = (i - 29):i,
                            i2 = (i + 1):(i + 30), 
                            df1 = 3, 
-                           df2 = 3)$lambda)
+                           df2 = 3)$lambda, error = function(e) return(NA)))
 }
 dates <- dates[30:(length(test) - 30 + 1)]
 
 filename <- "figures/LRT_scan_figure.pdf"
 pdf(filename, width = 6, height = 6)
 
+par(mfrow = c(2, 1))
 plot(lrt_stat ~ dates, type = "l", xlab = "Date", ylab = "LRT Statistic")
-abline(h = qchisq(0.95, df = 1), col = 2, lwd = 2)
+plot(test[30:(length(test) - 30 + 1)] ~ dates, col = "red", type = "l")
 
 dev.off()
--- code/vis_empirical_increase.R
+++ code/vis_empirical_increase.R
@@ -1,4 +1,5 @@
 load("./data/processed_dat.Rdata")
+
 load("./data/simulated_curves.Rdata")
 
 filename <- "figures/empirical_increase.pdf"
@@ -6,18 +7,29 @@ filename <- "figures/empirical_increase.pdf"
 pdf(filename, width = 6, height = 6)
 
 american_thanksgiving <- as.Date("2020-11-26")
+
 start_date <- american_thanksgiving - 29
+
 end_date <- american_thanksgiving + 30
+
 days <- seq(start_date, end_date, by = "day")
 
 par(mfrow = c(2,2))
-plot(new_cases_subset[1862, ] ~ days,  ylab = "Daily Incidence in FIPS 36067 (Wald test p-value = 0.017)", xlab = "Date") 
+
+plot(new_cases_subset[1862, ] ~ days,  ylab = "Daily Incidence in FIPS 36067 (Wald test p-value = 0.017)", 
+     xlab = "Date", ylim = c(0, 1400)) 
 abline(v = american_thanksgiving, col = 2)
-plot(new_cases_subset[1863, ] ~ days, ylab = "Daily Incidence in FIPS 36069 (Wald test p_value = 0.099)", xlab ="Date") 
+
+plot(new_cases_subset[1863, ] ~ days, ylab = "Daily Incidence in FIPS 36069 (Wald test p_value = 0.099)", 
+     xlab ="Date", ylim = c(0, 1400)) 
 abline(v = american_thanksgiving, col = 2)
-plot(curves[220, ] ~ days, ylab = "Simulated Daily Incidence (Wald test p-value = 0.004)", xlab = "Date")
+
+plot(curves[220, ] ~ days, ylab = "Simulated Daily Incidence (Wald test p-value = 0.004)", 
+     xlab = "Date", ylim = c(0, 1400))
 abline(v = american_thanksgiving, col = 2)
-plot(curves[212, ] ~ days, ylab = "Simulated Daily Incidence (Wald test p-value = 0.260)", xlab = "Date")
+
+plot(curves[212, ] ~ days, ylab = "Simulated Daily Incidence (Wald test p-value = 0.260)", 
+     xlab = "Date", ylim = c(0, 1400))
 abline(v = american_thanksgiving, col = 2)
 
 dev.off()

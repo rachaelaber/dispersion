de7ca526a726efaf66464444b51c6a9c27819b3b
rachaelaber <57012430+rachaelaber@users.noreply.github.com>
2023-08-10 15:05:02 -0700

    clean text

--- code/disp_stats.R
+++ code/disp_stats.R
@@ -1,5 +1,9 @@
 # Compute dispersion stat (deviance divided by degrees of freedom) for each county
 
+library(splines)
+
+load("data/processed_dat.Rdata")
+
 disp_stats <- rep(NA, times = nrow(new_cases_subset))
 
 for (i in 1:nrow(new_cases_subset)){
@@ -8,10 +12,15 @@ for (i in 1:nrow(new_cases_subset)){
   
   inds = 1:length(y)
   
-  pop = rep(populations$population[i], times = length(y))
+  pop = rep(populations_subset$population[i], times = length(y))
   
   pois.fit <- glm(y ~ ns(inds, df = 3, intercept = TRUE) + offset(log(pop)), 
                   family = poisson)
+  
   disp_stats[i] = pois.fit$deviance/(length(y) - 3)
   
 }
+
+filename <- "data/dispersion_stats.Rdata"
+
+save(disp_stats, file = filename)

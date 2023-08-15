73b0f027c51637fd9af74ccb60f368f7de096ea9
rachaelaber <57012430+rachaelaber@users.noreply.github.com>
2023-08-15 12:56:08 -0700

    changes

--- code/diagnostic_plot.2.R
+++ code/diagnostic_plot.2.R
@@ -1,38 +1,40 @@
-# Check whether dispersion well represented by negative binomial model - just using the NY and FL counties 
-# with significant LRT result
+# Check whether dispersion is well represented by NB model 
+# using FL counties with significant LRT result
 
 source("code/W.R")
 
 load("./data/processed_dat.Rdata")
 
 load("./data/LRT_pvalsFL.Rdata")
-FL_index <- which(populations$State == "FL")
+FL_index <- which(populations_subset$State == "FL")
 FL_index <- FL_index[(which(pvals < 0.05))]
 
-load("./data/LRT_pvalsNY.Rdata")
-NY_index <- which(populations$State == "NY")
-NY_index <- NY_index[(which(pvals < 0.05))]
-
-signif_index <- c(NY_index, FL_index)
 state_fits <- c()
 
-for (i in signif_index){
-  state_fits <- c(state_fits, my_spl_fit(Y = new_cases_subset[i,], population = populations$population[i], 
+for (i in FL_index){
+  state_fits <- c(state_fits, my_spl_fit(Y = new_cases_subset[i,], population = populations_subset$population[i], 
                               inds = 1:60, df = 3))
 }
 
-state_fits <- matrix(state_fits, nrow = length(signif_index), ncol = 7, byrow = TRUE)
-state_fits <- cbind(state_fits, new_cases_subset[signif_index,])
+state_fits <- matrix(state_fits, nrow = length(FL_index), ncol = 7, byrow = TRUE)
+state_fits <- cbind(state_fits, new_cases_subset[FL_index,])
 state_fits <- as.data.frame(state_fits)
 colnames(state_fits) <- c("kappa", "beta", "mu", "l", "j", "theta", "SE.theta")
 
 
 # Plot squared residuals against squared fitted values
-par(mfrow = c(2, 7))
-for (i in 1:length(signif_index)){   
+
+filename <- "figures/diagnostic_plot.2.pdf"
+
+pdf(file = filename, width = 6, height = 6)
+
+par(mfrow = c(2, 3))
+
+for (i in 1:length(FL_index)){   
   plot(state_fits$mu[[i]]^2, (unlist(state_fits[i, 8:67]) - state_fits$mu[[i]])^2, 
        col = 2, xlab = "Squared Fitted Values", ylab = "Squared Residuals")
 }
 
+dev.off()
 # If the constant dispersion model (NB2) model is OK, then we should see a linear trend. 
 # If that checks out, then we don’t have to consider more complicated models.
--- code/vis_signific_state.R
+++ code/vis_signific_state.R
@@ -1,24 +1,28 @@
-# Visualize fitted curves in NY and FL counties where significant LRT result observed
+# Visualize fitted curves in FL counties where significant LRT result observed
 
 source("./code/W.R")
+
 load("./data/processed_dat.Rdata")
 
 load("./data/LRT_pvalsFL.Rdata")
-FL_index <- which(populations$State == "FL")
+
+FL_index <- which(populations_subset$State == "FL")
+
 FL_index <- FL_index[(which(pvals < 0.05))]
 
-load("./data/LRT_pvalsNY.Rdata")
-NY_index <- which(populations$State == "NY")
-NY_index <- NY_index[(which(pvals < 0.05))]
+filename <- "figures/FL_signif_fittedcurves.pdf"
 
-signif_index <- c(NY_index, FL_index)
+pdf(filename, width = 6, height = 6)
 
-par(mfrow = c(2, 7))
-for (i in signif_index){
+par(mfrow = c(2, 3))
+
+for (i in FL_index){
   
-  mu <- my_spl_fit(Y = new_cases_subset[i,], population = populations$population[i], 
-                   inds=1:length(new_cases_subset[i,]), df = 3)$mu
+  mu <- my_spl_fit(Y = new_cases_subset[i,], population = populations_subset$population[i], 
+                   inds = 1:length(new_cases_subset[i,]), df = 3)$mu
   
-  plot(1:length(new_cases_subset[i,]), new_cases_subset[i,])
+  plot(1:length(new_cases_subset[i,]), new_cases_subset[i,], xlab = , ylab = )
   lines(mu, col=2)
 }
+
+dev.off()

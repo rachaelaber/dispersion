4c3d4e4ccbed574380f2cee7f9407f0dd0b619b0
Benjamin Dalziel <benjamin.dalziel@oregonstate.edu>
2024-01-17 14:43:33 -0800

    sandbox.R shows clear results

--- code/sandbox.R
+++ code/sandbox.R
@@ -9,7 +9,7 @@ load("data/lrt_lg_pops_weekly.Rdata")
 load("data/new_cases_lg_weekly.Rdata")
 load("data/theta_lg_pops_weekly.Rdata")
 load("data/lrtps_lg_pops_weekly.Rdata")
-load("data/processed_long_dat.Rdata")  
+load("data/processed_long_dat.Rdata")
 
 
 # Trim elements or columns that will have NAs for theta and lrt
@@ -42,11 +42,19 @@ i <- 100
 par(mfrow = c(2, 2))
 
 x <- colMeans(new_cases_lg_weekly)
-plot(dates, x, type = 'l')
+plot(dates, x / sum(x, na.rm = T), type = 'l', lwd = 3)
+
+x <- new_cases_lg_weekly[1, ]
+lines(dates, x / sum(x, na.rm = T), col = 2)
+
+x <- new_cases_lg_weekly[100, ]
+lines(dates, x / sum(x, na.rm = T), col = 2)
 
 
-x <- clamp(thetas[i,], 0, 100)
-plot(dates, x)
+
+x <- clamp(thetas, 0, 10000)
+x <- colMeans(x, na.rm = TRUE)
+plot(dates, x, type = 'l', lwd = 3)
 
 # reasoning for clamping:
 # as theta increases much beyond 30, it is not practically different
@@ -58,10 +66,27 @@ image(t(clamp(thetas, 0, 100)), col = rev(viridis(32)))
 
 
 #
-image(t((lrt_ps)))
-legend("topright", cex = 0.5, bg = "white", legend = seq(round(min(lrt_ps, na.rm = TRUE), 2), 
-                                round(max(lrt_ps, na.rm = TRUE), 2), 
-                                length = 11), 
-       fill = heat.colors(10))
+
+x <- lrt_ps
+lower_range <- c(0, 10^seq(-50, -2, len = 12))
+upper_range <- c(1/20, seq(0.1, 1, len = 8))
+breakpoints <- c(lower_range, upper_range)
+nbreakpoint <- length(breakpoints)
+
+cols <- rev(cividis(nbreakpoint - 1))
+x[is.na(x)] <- 1
+image(dates,
+      1:nrow(x),
+      t(x),
+      col = cols,
+      breaks = breakpoints,
+      xaxt = "n")
+
+at <- seq.Date(from = min(dates), to = max(dates), by = 'month')
+labels <- format(at, format = "%b")
+labels <- substr(labels, 1, 1)
+
+axis.Date(1, at = at, labels = labels)
+
 
 dev.off()

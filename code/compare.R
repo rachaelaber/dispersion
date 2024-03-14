8b97c5166dd2f4c3b56e4bc42a5efc2adeec9091
rachaelaber <57012430+rachaelaber@users.noreply.github.com>
2024-03-14 16:31:24 -0700

    change png

--- code/compare.R
+++ code/compare.R
@@ -1,6 +1,8 @@
-# Note that the first 24 are omitted in these plots
+# Note that the first observations are omitted in these plots
 
 load("data/processed_long_dat.Rdata")
+rm(new_cases)
+rm(populations)
 
 load("data/new_cases_lg.Rdata")
 
@@ -8,24 +10,33 @@ load("data/lrt_lg_pops.Rdata")
 
 load("data/theta_lg_pops.Rdata")
 
-dates <- dates[30:(length(new_cases[1,]) - 30 + 1)]
-
 filename <- "figures/compare.png"
 
 png(filename)
 
-for (i in 1:1){ # 1:length(populations_lg$population)d
+# Trim to same length as outputs
+series <- new_cases_lg[1, 30:(length(new_cases_lg[1,]) - 30 + 1)]
+
+dates <- dates[30:(length(new_cases_lg[1,]) - 30 + 1)]
+
+# Remove first observations from all
+
+series <- series[35:length(series)]
+
+dates <- dates[35: length(dates)]
+
+lrt_stats <- lrt_stats[1, 35:dim(lrt_stats)[2]]
+
+thetas <- thetas[1, 35:dim(thetas)[2]]
+
+# Plot
+
+par(mfrow = c(3, 1))
   
-  series = new_cases_lg[i,]
+plot(dates, series, type = "h", ylab = "", main = "Incidence")
   
-  par(mfrow = c(3, 1))
+plot(dates, lrt_stats, type = "h", col = 2, ylab = "", main = "LRT")
   
-  plot(series[(30 + 35):(length(series) - 30 + 1)], type = "h", main = "Incidence")
-  
-  plot(lrt_stats[i, 35:dim(lrt_stats)[2]], type = "h", col = 2, main = "LRT")
-  
-  plot(log(thetas[i, 35:dim(thetas)[2]]), type = "l", col = 4, main = "Log Theta")
-  
-}
+plot(dates, log(thetas), type = "l", col = 4, ylab = "", main = "Log Theta")
 
 dev.off()
